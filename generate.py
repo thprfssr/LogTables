64078857f5c7f8104bccd2a205edc865737582d3
Javier Castro <jaca2899@gmail.com>
2019-12-07 15:56:22 -0500

    Wrote a function which takes an integer and converts it into another base, in the form of a string.

--- generate.py
+++ generate.py
@@ -16,3 +16,24 @@ def print_table(table, base):
     for i in range(0, len(table) // base):
         j = base * i
         print(table[j : j + base])
+
+def convert_to_base(x, base):
+    def _convert_to_base(x, base):
+        digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        result = ""
+        if base == 12:
+            digits = "0123456789XE"
+        elif base > 36:
+            print("Error! Base is larger than 36. Exiting...")
+            exit(-1)
+
+        if x == 0:
+            return "0"
+        while x != 0:
+            i = x % base
+            result = digits[i] + result
+            x = x // base
+
+        return result
+    f = np.vectorize(_convert_to_base)
+    return f(x, base)
