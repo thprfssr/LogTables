269ba13d7c6024b4cca7ec99b401bbdc8be3acd6
Javier Castro <jaca2899@gmail.com>
2019-12-07 15:50:12 -0500

    Defined the `get_log()` function, which takes the logarithm in a given base of an array of numbers, and converts it into integers which can be used in a table of logarithms.

--- generate.py
+++ generate.py
@@ -4,3 +4,10 @@ import numpy as np
 def get_log_input(base):
     x = [i / base**2 for i in range(base**2, base**2)]
     return np.array(x)
+
+def get_log(base):
+    x = get_log_input(base)
+    y = np.log(x) / np.log(base)
+    y *= base**4
+    y = np.round(y)
+    return y.astype(int)
