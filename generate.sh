35f9d7085db603409a564fc0538521c239452d66
Javier Castro <jaca2899@gmail.com>
2019-12-08 16:40:27 -0500

    Modified the `generate.sh` to call the new `sanitize.sed` script, and to generate both logarithms and interpolations with a single function.

--- generate.sh
+++ generate.sh
@@ -1,19 +1,35 @@
 #!/bin/bash
 
 logarithms_script='logarithms.py'
-logarithm_sanitize='logarithm_sanitize.sed'
+sanitize='sanitize.sed'
 
-# args: base
-function generate_logarithms
+# If the mode is "logarithm", then the function prints the columns of
+# logarithms. If the mode is "interpolation", then the function prints the
+# columns of the mean differences. Otherwise, the program dies.
+# args: mode, base
+# mode: "logarithm", "interpolation"
+function generate
 {
-	base=$1
+	mode=$1
+	base=$2
+	if [[ "$mode" == "logarithms" ]]; then
+		func=get_log
+		n=4
+	elif [[ "$mode" == "interpolations" ]]; then
+		func=get_interpolation
+		n=1
+	else
+		echo Unrecognized mode \"$mode\". Exiting...
+		exit -1
+	fi
+
 	str=$(python <<EOF
 from $(basename $logarithms_script .py) import *
-x = get_log($base)
+x = $func($base)
 x = convert_to_base(x, $base)
-x = sanitize_numeric_string(x, 4)
+x = sanitize_numeric_string(x, $n)
 print_table(x, $base)
 EOF
 )
-	echo $str | tr -d "\n" | sed -f $logarithm_sanitize
+	echo $str | tr -d "\n" | sed -f $sanitize
 }
